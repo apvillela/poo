cd1ea213594cf68ca4fefd864f153e12ad268eae
Alexandre Villela <ap.villela10@gmail.com>
2024-11-28 19:16:33 -0300

    ultimo commit desse exercicio

--- aula14/app/src/main/java/org/example/SistemaGeradorDeSenhas.java
+++ aula14/app/src/main/java/org/example/SistemaGeradorDeSenhas.java
@@ -2,34 +2,41 @@ package org.example;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SistemaGeradorDeSenhas {
 
-    private ArrayList<Senha> senhas;
+    private Queue<Senha> senhas;
     private AtomicInteger contador;
 
     public SistemaGeradorDeSenhas() {
-        this.senhas = new ArrayList<>();
+        this.senhas = new LinkedList<>();
         this.contador = new AtomicInteger();
 
     }
 
     public void adicionarSenha() {
+        LocalDateTime horario = LocalDateTime.now();
         Integer numero = contador.incrementAndGet();
+
+        Senha s = new Senha(numero, false, "conta", horario);
+        this.senhas.add(s);
 
     }
 
     public Senha chamarProxSenha() {
-
+        return this.senhas.poll();
     }
 
     public static void main(String[] args) {
+        SistemaGeradorDeSenhas novo = new SistemaGeradorDeSenhas();
 
-        SistemaGeradorDeSenhas s = new SistemaGeradorDeSenhas();
-        s.adicionarSenha();
-        Senha a = s.chamarProxSenha();
-
+        novo.adicionarSenha();
+        novo.chamarProxSenha();
+        // novo.chamarProxSenha();
+        
     }
 
 }
